5a39b0e821a6b1295097695510f90a2a6de1ddb5
Charles Anthony <anthony@cidanmachinery.com>
2023-10-24 23:08:39 +0200

    dummy message example

--- rfc3339format_example.js
+++ rfc3339format_example.js
@@ -1,6 +1,30 @@
 async function fetchData(fromDate, toDate) {
-    const tofrc3339 = (date) => date.toISOString()
+    const torfc3339 = (date) => date.toISOString()
 
-    const baseurl = null
+    const baseurl = "https://example.com/data"
+
+    const url = `${baseURL}?fromDate=${torfc3339(fromDate)}&toDate=${torfc3339(toDate)}`
+
+    try {
+        const response = await fetch(url)
+
+        if(!response.ok) {
+            throw new Error(`http error!, Status: ${response.status}`)
+        }
+        const data = await response(data)
+        return data
+
+        
+    } catch(data) {
+        console.error("There was problem with the fetch: ", error)
+    }
 }
 
+
+const startDate = new Date('2023-01-01T00:00:00Z')
+const endDate = new Date('2023-05-01T00:00:00Z')
+
+
+fetchData(startDate, endDate).then(data => {
+    console.log(data)
+})
